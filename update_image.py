24c18d40e30a8e010d6cecff46439b928dc3b1b1
NoAnyLove <noanylove@gmail.com>
2017-08-07 18:29:46 -0400

    Implement incremental update image
    Using websocket to get the incremental update message

--- update_image.py
+++ update_image.py
@@ -1,15 +1,47 @@
+import collections
 import requests
+import struct
 import threading
 import time
 import os
+import json
 from datetime import datetime
 from PIL import Image
+import websocket
 from util import code_map, hex_to_rgb
 
 
 __all__ = ["UpdateImage"]
 
 url = r"http://api.live.bilibili.com/activity/v1/SummerDraw/bitmap"
+
+token = bytearray([0x00, 0x00, 0x00, 0x27, 0x00, 0x10, 0x00, 0x01, 0x00, 0x00,
+                   0x00, 0x07, 0x00, 0x00, 0x00, 0x01, 0x7B, 0x22, 0x75, 0x69,
+                   0x64, 0x22, 0x3A, 0x30, 0x2C, 0x22, 0x72, 0x6F, 0x6F, 0x6D,
+                   0x69, 0x64, 0x22, 0x3A, 0x35, 0x34, 0x34, 0x36, 0x7D])
+
+heart_beat = bytearray([0x00, 0x00, 0x00, 0x10, 0x00, 0x10, 0x00, 0x01, 0x00,
+                        0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x01])
+
+"""
+Protocol:
+
+4 - JSON message end offset
+2 - JSON message start offset
+2 - unknown1
+4 - opcode
+    3 - something for online
+    5 - msg
+4 - unknown2, padding?
+"""
+message_header_struct = struct.Struct("!IHHI")
+MessageHeader = collections.namedtuple("MessageHeader",
+                                       [
+                                           "end_offset",
+                                           "start_offset",
+                                           "unknown1",
+                                           "opcode",
+                                       ])
 
 
 class UpdateImage(object):
@@ -25,6 +57,15 @@ class UpdateImage(object):
         self.autosave = autosave
         self.lazy_threshold = lazy_threshold
         self.timeout = 15
+        self.ws = websocket.WebSocketApp(
+            r"ws://broadcastlv.chat.bilibili.com:2244/sub",
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close)
+        self.ws.on_open = self.on_open
+        self.ws_loop_thread = None
+        self.heart_beat_thread = None
+        self.enable_reconnect = True
 
     def get_auto_save_filename(self):
         return self.filename_template.format(datetime.now())
@@ -139,6 +180,132 @@ class UpdateImage(object):
         with self.sync_lock:
             task = func(self)
         return task
+
+    def on_open(self, ws):
+        ws.send(token)
+
+        # Avoid creating multiple heart beat thread
+        if self.heart_beat_thread and self.heart_beat_thread.is_alive():
+            return
+
+        self.heart_beat_thread = threading.Thread(
+            target=self.thread_func_heart_beat)
+        self.heart_beat_thread.daemon = True
+        self.heart_beat_thread.start()
+
+    def on_message(self, ws, message):
+        try:
+            message_header = MessageHeader._make(
+                message_header_struct.unpack_from(message)
+            )
+            if message_header.opcode == 3:
+                print("[INFO] online message")
+            elif message_header.opcode == 5:
+                print("[INFO] receive %d bytes data for decoding" %
+                      len(message))
+                self.process_message(message)
+            elif message_header.opcode == 8:
+                print("[INFO] receive heart beat request")
+                self.heart_beat()
+            else:
+                print("[INFO] Unkown opcode %d" % message_header.opcode)
+        except Exception as e:
+            print("[ERROR] cannot decode message format: %s" % e)
+            return
+
+    def process_message(self, message):
+        update_list = []
+        offset = 0
+        while offset < len(message):
+            try:
+                message_header = MessageHeader._make(
+                    message_header_struct.unpack_from(message, offset)
+                )
+                data = message[offset + message_header.start_offset:offset +
+                               message_header.end_offset]
+                message_object = json.loads(data)
+
+                cmd = message_object['cmd']
+                if cmd == "DRAW_UPDATE":
+                    x = message_object['data']['x_max']
+                    y = message_object['data']['y_max']
+                    color_code = message_object['data']['color']
+                    rgb = hex_to_rgb(code_map[color_code])
+                    update_list.append([x, y, rgb])
+
+                    print("@%s, cmd: %s, update (%d, %d) with color %s" %
+                          (datetime.now(), cmd, x, y, color_code))
+                else:
+                    print("@%s, Other message: %s" % (datetime.now(), data))
+
+                offset += message_header.end_offset
+
+            except Exception as e:
+                print("Error occurs in process_message: %s" % e)
+                print("Error message(offset: %d): %s," % (offset, message))
+                print("Error message(bytearray): %r" % bytearray(message))
+                break
+
+        # finally update the pixels in critical section
+        start_time = time.clock()
+        with self.sync_lock:
+            for x, y, rgb in update_list:
+                self.set_image_pixel(x, y, rgb)
+        print("[DEBUG] update pixels in %.6f" % (time.clock() - start_time))
+
+    def on_error(self, ws, error):
+        """
+        Generally speaking, on_close will be invoked after on_error
+        """
+        print("[ERROR]@%s, on_error is called with: %s" %
+              (datetime.now(), error))
+
+    def on_close(self, ws):
+        """
+        We need rerun the WebSocket loop in another thread. Because we are
+        currently at the end of a WebSocket loop running inside
+        self.ws_loop_thread.
+
+        DO NOT join on that thread, that is the current thread
+        """
+        print("[ERROR]@%s, on_close is called" % (datetime.now()))
+        print("[INFO] WebSocket is closed after running %.2f seconds" %
+              (time.time() - self.start_time))
+
+        # on_close means the WebSocket loop thread is terminated, we restart
+        # the loop
+        if self.enable_reconnect:
+            print("[INFO] restart WebSocket connection")
+            self.update_image_with_incremental_update()
+
+    def heart_beat(self):
+        print("[DEBUG] Sending heart beat")
+        self.ws.send(heart_beat)
+
+    def start_websocket(self):
+        # DO NOT join on the self.ws_loop_thread, it's the current thread.
+        # Just create a new thread
+        self.ws_loop_thread = threading.Thread(target=self.ws.run_forever)
+        self.ws_loop_thread.daemon = True
+        self.ws_loop_thread.start()
+
+    def thread_func_heart_beat(self):
+        while True:
+            try:
+                self.heart_beat()
+            except websocket.WebSocketConnectionClosedException:
+                print("[ERROR] WebSocket connection is already closed")
+                break
+            except Exception as e:
+                print("[ERROR] heart beat thread has error: %e" % e)
+                break
+            time.sleep(30)
+        print("[INFO] heart beat threat exit")
+
+    def update_image_with_incremental_update(self):
+        self.start_time = time.time()
+        self.update_image()
+        self.start_websocket()
 
 
 def convert_code_to_bytes(code_map, code_data, buf):
