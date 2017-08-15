a01a7e408cd9d687dac44aab06fa2640ba1f96b0
NoAnyLove <noanylove@gmail.com>
2017-08-15 14:48:47 -0400

    Fix record.py with AsyncUpdateImage

--- record.py
+++ record.py
@@ -1,20 +1,45 @@
-import time
+import asyncio
+import os
 from datetime import datetime
-from update_image import UpdateImage
+from async_update_image import AsyncUpdateImage
 
 interval = 180
+filename_template = "autosave/autosave_{:%Y_%m_%d-%H_%M_%S}.gif"
 
 
-def log():
-    print("@%s record current image" % datetime.now())
+def print_log(filename):
+    print("@%s record current image, save file %s" %
+          (datetime.now(), filename))
 
+
+async def wakeup():
+    """ Keep script responsive to Ctrl+C
+    """
+    while True:
+        await asyncio.sleep(1)
+
+
+async def recording(up):
+    while True:
+        await up.async_update_image()
+        filename = filename_template.format(datetime.now())
+        up.save_buffer_to_file(filename)
+        print_log(filename)
+        await asyncio.sleep(interval)
 
 if __name__ == "__main__":
-    up = UpdateImage(True)
+    if not os.path.exists('autosave'):
+        print("Create output folder autosave")
+        os.makedirs("autosave")
+
+    up = AsyncUpdateImage()
     print("Start @%s" % datetime.now())
-    log()
-    up.update_image()
-    while True:
-        time.sleep(interval)
-        log()
-        up.update_image()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(asyncio.gather(recording(up), wakeup()))
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed, existing")
+    finally:
+        up.close()
+        loop.stop()
+        loop.close()
